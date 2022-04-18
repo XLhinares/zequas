fbd9d99833383f4b0082c7db907b1bef7f4063bf
Xavier Lhinares <xlhinares@gmail.com>
2022-04-18 18:47:55 +0200

    Added: launcher icon

--- lib/tabs/game.dart
+++ lib/tabs/game.dart
@@ -53,9 +53,11 @@ class TabGame extends StatelessWidget {
               builder: (_) => Column(
                 children: [
                   Expanded(
+                    // FrameFit does not appear earlier in the tree so that the
+                    // progress bar can fill the whole width.
                     child: FrameFit(
                       padding: EdgeInsets.symmetric(
-                        horizontal: Get.width * 0.1,
+                        horizontal: xPaddingM,
                       ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
